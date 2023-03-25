4313ec0aba2361e293e0fc65c6fa9c382b1de3b3
UPMANYU SHARMA <upmanyusharma31@gmail.com>
2023-03-25 19:43:42 +0530

    Newsify project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React, { useState } from "react";
+import NavBar from "./Components/NavBar";
+import News from "./Components/News";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+  const apiKey = process.env.REACT_APP_MY_KEY
+
+  const [progress, setProgress] = useState(0)
+
+    return (
+      <div>
+        <Router>
+          <LoadingBar
+            color="#f11946"
+            progress={progress}
+            height={4}
+          />
+          <NavBar />
+          <Routes>
+            {/* key isliye use taki vo update kre jb bhi category pe click kre */}
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="general"
+                  pageSize={9}
+                  country="in"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="business"
+                  pageSize={9}
+                  country="in"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="entertainment"
+                  pageSize={9}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/general"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="general"
+                  pageSize={9}
+                  country="in"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="health"
+                  pageSize={9}
+                  country="in"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="science"
+                  pageSize={9}
+                  country="in"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="sports"
+                  pageSize={9}
+                  country="in"
+                  category="sports"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  apiKey={apiKey}
+                  changeProgress={setProgress}
+                  key="technology"
+                  pageSize={9}
+                  country="in"
+                  category="technology"
+                />
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    );
 }
-
 export default App;
